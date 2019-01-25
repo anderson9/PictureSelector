45b4413f875b05e2a5689d51214568b72e2d4446
luojiusan <328701143@qq.com>
2019-01-25 11:53:09 +0800

    修改视频录制 跳转到选择视频

--- picture_library/src/main/java/com/luck/picture/lib/camare/TakeCamareActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/camare/TakeCamareActivity.java
@@ -37,12 +37,15 @@ public class TakeCamareActivity extends AppCompatActivity {
         intent.putExtra("requeCode", requeCode);
         mActivity.startActivityForResult(intent, requeCode);
     }
+
     String cameraPath;
     int requeCode;
+    public static final String TOSELECT = "toselect";
 
     private final int GET_PERMISSION_REQUEST = 100; //权限申请自定义码
     private JCameraView jCameraView;
     private boolean granted = false;
+    public static final int REQUEST_VEDIO_CUSTOM = 105;//自定义录制的视频 跟益村耦合了一个编码
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,25 +86,31 @@ public class TakeCamareActivity extends AppCompatActivity {
         jCameraView.setRightClickListener(new ClickListener() {
             @Override
             public void onClick() {
-                PictureSelector.create(TakeCamareActivity.this)
-                        .openGallery(PictureMimeType.ofVideo())
-                        .theme(R.style.picture_default_style)
-                        .maxSelectNum(1)
-                        .imageSpanCount(4)
-                        .selectionMode(PictureConfig.SINGLE)
-                        .previewVideo(true)
-                        .previewImage(true)// 是否可预览图片 true or false
-                        .isCamera(true)// 是否显示拍照按钮 true or false
-                        .compress(true)// 是否压缩 true or false
-                        .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
-                        .isGif(true)// 是否显示gif图片 true or false
-                        .openClickSound(false)// 是否开启点击声音 true or false
-                        .previewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
-                        .cropCompressQuality(90)// 裁剪压缩质量 默认90 int
-                        .videoQuality(1)// 视频录制质量 0 or 1 int
-                        .videoMaxSecond(180)// 显示多少秒以内的视频or音频也可适用 int
-                        .recordVideoSecond(20)//视频秒数录制 默认60s int
-                        .forResult(requeCode);//结果回调onActivityResult code
+                if (requeCode == REQUEST_VEDIO_CUSTOM) {//表示取消录制 需要源activity去跳转选择视频
+                    Intent intent = new Intent();
+                    intent.putExtra("url", TOSELECT);
+                    setResult(Activity.RESULT_OK, intent);
+                } else { //直接跳转选择视频
+                    PictureSelector.create(TakeCamareActivity.this)
+                            .openGallery(PictureMimeType.ofVideo())
+                            .theme(R.style.picture_default_style)
+                            .maxSelectNum(1)
+                            .imageSpanCount(4)
+                            .selectionMode(PictureConfig.SINGLE)
+                            .previewVideo(true)
+                            .previewImage(true)// 是否可预览图片 true or false
+                            .isCamera(true)// 是否显示拍照按钮 true or false
+                            .compress(true)// 是否压缩 true or false
+                            .sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                            .isGif(true)// 是否显示gif图片 true or false
+                            .openClickSound(false)// 是否开启点击声音 true or false
+                            .previewEggs(true)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中) true or false
+                            .cropCompressQuality(90)// 裁剪压缩质量 默认90 int
+                            .videoQuality(1)// 视频录制质量 0 or 1 int
+                            .videoMaxSecond(180)// 显示多少秒以内的视频or音频也可适用 int
+                            .recordVideoSecond(20)//视频秒数录制 默认60s int
+                            .forResult(requeCode);//结果回调onActivityResult code
+                }
                 finish();
             }
         });
